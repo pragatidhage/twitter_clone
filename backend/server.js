30ce46ba2be6194f328706d399401a6c1a52a3d4
Pragati Dhage <pragatidhage971@gmail.com>
2024-07-08 19:33:37 +0530

    dependencies and connected to mongo

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+console.log(process.env.MONGO_URI);
+
+app.use("/api/auth",authRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
+    connectMongoDB();
+});
