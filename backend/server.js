e179299ef70703ee758370082d20208d028d5c0a
Pragati Dhage <pragatidhage971@gmail.com>
2024-07-14 17:35:48 +0530

    created signup api aswell as jwt token auth

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 console.log(process.env.MONGO_URI);
 
+app.use(express.json());//to parse req.body
+app.use(express.urlencoded({extended : true}));
+
 app.use("/api/auth",authRoutes);
 
 app.listen(PORT,()=>{
