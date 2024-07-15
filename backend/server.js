838a92fa4c45b6f18519dafe492aebf1c2069226
Pragati Dhage <pragatidhage971@gmail.com>
2024-07-16 00:31:24 +0530

    api for follow unfollow added

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
@@ -17,6 +19,7 @@ app.use(express.urlencoded({extended : true}));
 
 app.use(cookieParser());
 app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes)
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
