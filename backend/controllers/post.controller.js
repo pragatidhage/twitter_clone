7cc83de0de0915468244f18e8d22c45185900232
Pragati Dhage <pragatidhage971@gmail.com>
2024-07-17 00:04:49 +0530

    endpoint for likedposts from following

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -171,3 +171,28 @@ export const getLikedPosts = async (req,res)=>{
     }
 
 }
+
+export const getFollowingPosts = async (req,res) =>{
+    try{
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if(!user) return res.status(404).json({error:"User not found"});
+
+        const following = user.following;
+
+        const feedPosts = await Post.find({user: {$in:following}})
+        .sort({ createdAt:-1})
+        .populate({
+            path:"user",
+            select:"-password",
+        })
+        .populate({
+            path:"comments.user",
+            select:"-password",
+        })
+        res.status(200).json(feedPosts);
+    }catch(error){
+        console.log("Error in getFollowingPosts controller",error);
+        res.status(500).json({error:"Inetrnal server error"});
+    }
+}
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
-import { commentOnPost, createPost, deletePost, getAllPost , getLikedPosts, likeUnlikePost } from '../controllers/post.controller.js';
+import { commentOnPost, createPost, deletePost, getAllPost , getFollowingPosts, getLikedPosts, likeUnlikePost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.get("/all",protectRoute,getAllPost);
+router.get("/following/:id",protectRoute,getFollowingPosts);
 router.get("/likes/:id",protectRoute,getLikedPosts)
 router.post("/create",protectRoute,createPost);
 router.post("/like/:id",protectRoute,likeUnlikePost);
