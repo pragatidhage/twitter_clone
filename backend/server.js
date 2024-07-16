b57e54fde3bd7abf3d4d2dcecb86bdf47e10a3db
Pragati Dhage <pragatidhage971@gmail.com>
2024-07-16 14:44:33 +0530

    update api added,user controller done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary} from "cloudinary";
 
 
 import authRoutes from "./routes/auth.routes.js";
@@ -9,6 +10,11 @@ import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 const app = express();
 const PORT = process.env.PORT || 5000;
