5b6f9fc7f0c3803ecde058d5e23910b2c9efcb87
Pragati Dhage <pragatidhage971@gmail.com>
2024-07-17 00:18:55 +0530

    endpoint to fetch all post by user , post controller done

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
-import { commentOnPost, createPost, deletePost, getAllPost , getFollowingPosts, getLikedPosts, likeUnlikePost } from '../controllers/post.controller.js';
+import { commentOnPost, createPost, deletePost, getAllPost , getFollowingPosts, getUserPosts,getLikedPosts, likeUnlikePost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.get("/all",protectRoute,getAllPost);
 router.get("/following/:id",protectRoute,getFollowingPosts);
-router.get("/likes/:id",protectRoute,getLikedPosts)
+router.get("/likes/:id",protectRoute,getLikedPosts);
+router.get("/user/:username",protectRoute,getUserPosts);
 router.post("/create",protectRoute,createPost);
 router.post("/like/:id",protectRoute,likeUnlikePost);
 router.post("/comment/:id",protectRoute,commentOnPost);
