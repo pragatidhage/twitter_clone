8eb4085a83c5144155ab24f5d3dc7a0200ed8891
Pragati Dhage <pragatidhage971@gmail.com>
2024-07-26 10:25:15 +0530

    img upload fixed

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 console.log(process.env.MONGO_URI);
 
-app.use(express.json());//to parse req.body
+app.use(express.json({limit:"5mb"}));//to parse req.body
 app.use(express.urlencoded({extended : true}));
 
 app.use(cookieParser());
