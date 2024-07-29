8b4bc74dfa8b9ea67b607654bc5a763c400bf5d6
Pragati Dhage <pragatidhage971@gmail.com>
2024-07-29 18:01:22 +0530

    like unlike post functionality fixed

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -102,7 +102,8 @@ export const likeUnlikePost = async (req,res) =>{
             
             await Post.updateOne({_id:postId}, {$pull:{likes: userId}});
             await User.updateOne({_id:userId},{ $pull:{ likedPosts:postId}});
-            res.status(200).json({message : "Post Unliked"});
+            const updatedLikes = post.likes.filter((id)=>id.toString() !== userId.toString());
+            res.status(200).json(updatedLikes);
         }else{
             
             post.likes.push(userId);
@@ -116,7 +117,9 @@ export const likeUnlikePost = async (req,res) =>{
             })
 
             await notification.save();
-            res.status(200).json({message : "Post Liked"});
+
+            const updatedLikes = post.likes
+            res.status(200).json(updatedLikes);
         }
 
 
