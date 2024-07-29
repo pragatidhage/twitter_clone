2351c97c7a5da036929159bfd15e52c7bac2a459
Pragati Dhage <pragatidhage971@gmail.com>
2024-07-29 23:27:46 +0530

    changes for deplloyment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary} from "cloudinary";
+import path from "path";
 
 
 import authRoutes from "./routes/auth.route.js";
@@ -20,6 +21,8 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
+
 console.log(process.env.MONGO_URI);
 
 app.use(express.json({limit:"5mb"}));//to parse req.body
@@ -30,6 +33,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notifications",notificationRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
+}
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
