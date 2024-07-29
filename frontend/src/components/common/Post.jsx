6e5c9c723654206b8afbadcdd721f6d8c9a53f18
Pragati Dhage <pragatidhage971@gmail.com>
2024-07-29 19:14:01 +0530

    comment functionality and create post fixed

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -9,10 +9,15 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 
 import LoadingSpinner from "./LoadingSpinner"
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
 	const {data:authUser} = useQuery({queryKey:["authUser"]});
+	const postOwner = post.user;
+	const isLiked = post.likes.includes(authUser._id);
+	const isMyPost = authUser._id === post.user._id;
+	const formattedDate = formatPostDate(post.createdAt);
 
 	const queryClient = useQueryClient();
 
@@ -71,14 +76,38 @@ const Post = ({ post }) => {
 		}
 	})
 
-	const postOwner = post.user;
-	const isLiked = post.likes.includes(authUser._id);
+	const { mutate:commentPost, isPending:isCommenting}= useMutation({
+		mutationFn:async()=>{
+			try{
+				const res = await fetch(`/api/posts/comment/${post._id}`,{
+					method:"POST",
+					headers:{
+						"Content-Type":"application/json",
+					},
+					body:JSON.stringify({text:comment}),
+				});
+				const data = await res.json();
+				if(!res.ok){
+					throw new Error(data.error || "Something went wrong")
+				}
+				return data
+			}catch(error){
+				throw new Error(error);
+			}
+		},
+		onSuccess:()=>{
+			toast.success("Comment posted successfully");
+			setComment("");
+			queryClient.invalidateQueries({queryKay:["posts"]});
+		},
+		onError:(error)=>{
+			toast.error(error.message);
+		}
+	})
 
-	const isMyPost = authUser._id === post.user._id;
+	
 
-	const formattedDate = "1h";
-
-	const isCommenting = false;
+	
 
 	const handleDeletePost = () => {
 		deletePost();
@@ -86,6 +115,8 @@ const Post = ({ post }) => {
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
+		if(isCommenting) return;
+		commentPost();
 	};
 
 	const handleLikePost = () => {
